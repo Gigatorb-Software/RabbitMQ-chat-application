4ac40d5dc8b71d634bb2b2999898cb3da0c021ec
alakh20 <alakhkarmarkar@gmail.com>
2021-08-24 14:11:41 +0530

    Initial commit

---
+++ src/main/java/com/gigatorb/controller/WebSocketChatController.java
@@ -0,0 +1,28 @@
+package com.gigatorb.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.gigatorb.domain.WebSocketChatMessage;
+
+@Controller
+public class WebSocketChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/gigatorb")
+    public WebSocketChatMessage sendMessage(@Payload WebSocketChatMessage webSocketChatMessage) {
+        return webSocketChatMessage;
+    }
+
+    @MessageMapping("/chat.newUser")
+    @SendTo("/topic/gigatorb")
+    public WebSocketChatMessage newUser(@Payload WebSocketChatMessage webSocketChatMessage,
+                                        SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", webSocketChatMessage.getSender());
+        return webSocketChatMessage;
+    }
+
+}
